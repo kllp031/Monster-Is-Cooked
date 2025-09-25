8dbf1e26287b374a87719eacc08621ca0417876a
NguyenThanhVu028 <23521811@gm.uit.edu.vn>
2025-09-25 20:44:45 +0700

    Upgrade Stove UI

---
+++ Assets/Scripts/Knight/InteractableDetector.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/* This class is used by the player, it detects any object that has the "Interactable" script attached
+   When player hit the specified "Interact button", simply call the "Interact()" function on the interable object
+   That object will handle the logic, not the player*/
+
+[RequireComponent(typeof(Collider2D))]
+public class InteractableDetector : MonoBehaviour
+{
+    [SerializeField] Button interactButton;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Interactable obj = collision?.GetComponent<Interactable>();
+        if (obj)
+        {
+            interactButton?.onClick.RemoveAllListeners();
+            interactButton?.onClick.AddListener(obj.OnInteract);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Interactable obj = collision?.GetComponent<Interactable>();
+        if (obj)
+        {
+            interactButton?.onClick.RemoveListener(obj.OnInteract);
+        }
+    }
+}
