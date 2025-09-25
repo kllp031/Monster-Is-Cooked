d8410131ce05ac6f52be37f06b64d880223e5359
kllp031 <31173206+kllp031@users.noreply.github.com>
2025-09-25 11:32:52 +0700

    Innitial UI desgin
    create ui design for joystick control, inventory, kitchen

--- Assets/Scripts/Knight/StoveInteract.cs
+++ Assets/Scripts/Knight/StoveInteract.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 
 public class StoveInteract : MonoBehaviour
 {
+    [SerializeField] private stoveUI;
     private GameObject interactableStove;
 
     public InputActionAsset actions;
