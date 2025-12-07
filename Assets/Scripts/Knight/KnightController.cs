f6840bee73a4dbcf98dc78a0d7e483bd7d81868b
ToanfKhoa <quachbuitoankhoa@gmail.com>
2025-12-07 10:56:22 +0700

    Fix sprite sheet, animation run and idle transition

--- Assets/Scripts/Knight/KnightController.cs
+++ Assets/Scripts/Knight/KnightController.cs
@@ -6,11 +6,34 @@ public class KnightController : MonoBehaviour
     private Vector2 moveInput = Vector2.zero;
 
     [SerializeField] private float moveSpeed = 5f;
+    private Animator animator;
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+
+        Debug.Log("moveinput" + moveInput);
+        //Animation
+        if (moveInput != Vector2.zero)
+            animator.SetBool("isRunning", true);
+        else
+            animator.SetBool("isRunning", false);
+
+        //Flip
+        if (moveInput.x > 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (moveInput.x < 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 
     private void FixedUpdate()
