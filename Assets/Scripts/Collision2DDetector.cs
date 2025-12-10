f10360395af0ce99883a11bdf7b57ec4bef2e8b1
NguyenThanhVu028 <23521811@gm.uit.edu.vn>
2025-12-10 21:48:30 +0700

    Fix throwing food mechanic
    Fix player's throwing food direction
    Prevent player from throwing food if there is an obstacle blocking
    Improve physics of the food, the food is no longer stuck in the table or out of the map when thrown

--- Assets/Scripts/Collision2DDetector.cs
+++ Assets/Scripts/Collision2DDetector.cs
@@ -4,26 +4,44 @@ using UnityEngine.Events;
 [RequireComponent(typeof(Collider2D))]
 public class Collision2DDetector : MonoBehaviour
 {
+    [SerializeField] bool useOverlap = false;
+    [SerializeField] LayerMask overlapMask;
+    [SerializeField] float overlapRange;
+    [SerializeField] Vector2 overlapOffset = new();
+
     [SerializeField] bool detectTrigger = true;
     [SerializeField] bool detectCollision = true;
+    [SerializeField] UnityEvent<Collider2D[]> onOverlapDetected = new();
     [SerializeField] UnityEvent<Collider2D> onTriggerEnter;
+    [SerializeField] UnityEvent<Collider2D> onTriggerStay;
     [SerializeField] UnityEvent<Collider2D> onTriggerExit;
     [SerializeField] UnityEvent<Collision2D> onCollisionEnter;
     [SerializeField] UnityEvent<Collision2D> onCollisionExit;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (detectTrigger) onTriggerEnter?.Invoke(collision);
+        if (detectTrigger && !useOverlap) onTriggerEnter?.Invoke(collision);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (detectTrigger && !useOverlap) onTriggerStay?.Invoke(collision);
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (detectTrigger) onTriggerExit?.Invoke(collision);
+        if (detectTrigger && !useOverlap) onTriggerExit?.Invoke(collision);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (detectCollision) onCollisionEnter?.Invoke(collision);
+        if (detectTrigger && !useOverlap) onCollisionEnter?.Invoke(collision);
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (detectCollision) onCollisionExit?.Invoke(collision);
+        if (detectTrigger && !useOverlap) onCollisionExit?.Invoke(collision);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!useOverlap) return;
+        var colliders = Physics2D.OverlapCircleAll((Vector2)transform.position + overlapOffset, overlapRange, overlapMask);
+        if(colliders != null) onOverlapDetected?.Invoke(colliders);
     }
 }
